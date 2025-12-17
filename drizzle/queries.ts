5c1eb4779eadccf8b58645c63c1972f76a5a6e1e
Emanuele Pavan <emanuele.pav@gmail.com>
2025-12-17 12:16:11 +0100

    implements quests && leaderboard

--- drizzle/queries.ts
+++ drizzle/queries.ts
@@ -220,3 +220,24 @@ export const getLessonPercentage = cache(async () => {
 
   return percentage;
 });
+
+export const getTopTenUsers = cache(async () => {
+  const session = await getSession();
+
+  if (!session?.user) {
+    return [];
+  }
+
+  const data = await db.query.userProgress.findMany({
+    orderBy: (userProgress, { desc }) => [desc(userProgress.points)],
+    limit: 10,
+    columns: {
+      userId: true,
+      username: true,
+      userImageSrc: true,
+      points: true,
+    },
+  });
+
+  return data;
+});
